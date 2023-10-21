8c05e862e7183cd867388654c527999068945eff
Amit Kumar Jha <77675138+AIM-IT4@users.noreply.github.com>
2023-10-21 15:59:29 +0530

    Add files via upload

---
+++ cdo_pricing.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm, sem, t
+
+# Initialize parameters and variables
+# [The entire Python code for the project will go here]
+
+# Monte Carlo simulation, plots, and calculations
+# [The entire Python code for the project will go here]
